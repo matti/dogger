bfb81ea85aae89efa3e9a3123fb3c496e2e54a65
Matti Paksula <matti.paksula@iki.fi>
2019-12-16 20:14:31 +0200

    :sunrise:

---
+++ lib/dogger.rb
@@ -0,0 +1,6 @@
+require "dogger/version"
+
+module Dogger
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/dogger_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe Dogger do
+  it "has a version number" do
+    expect(Dogger::VERSION).not_to be nil
+  end
+
+end
